3a296a81a6934f197ece832827901ec48b4126de
adnan reza <adnanireza@yahoo.com>
2021-10-27 14:54:41 +0000

    Implement array 1.2

--- src/1_2_array.cpp
+++ src/1_2_array.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+template <typename T>
+void disp(vector<T> v) {
+    for (auto elem : v) {
+        cout << elem << endl;
+    }
+}
+
+template <typename T>
+pair<int, int> unsorted_range(vector<T> v) {
+    int len = v.size();
+    int left = 0, right = len - 1;
+    T running_min = v[len - 1];
+    for (int i = len - 1; i >= 0; i--) {
+        if (v[i] < running_min) {
+            running_min = v[i];
+        } else {
+            left = i;
+        }
+    }
+    T running_max = v[0];
+    for (int i = 0; i < len; i++) {
+        if (v[i] > running_max) {
+            running_max = v[i];
+        } else {
+            right = i;
+        }
+    }
+    return make_pair(left, right);
+}
+
 int main() {
-    cout << "22222222222222" << endl;
+    vector<int> v {4, 7, 8, 19, 60, 7, 55, 89};
+    auto res = unsorted_range(v);
+    cout << res.first << ", " << res.second << endl;
 }
