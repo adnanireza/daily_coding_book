bac26d6f841039c1b610dc8340784fd42bbc23bb
adnan reza <adnanireza@yahoo.com>
2022-08-26 15:10:26 +0000

    Implement BST 7.1: Floor and ceiling

--- src/7_0_bst.cpp
+++ src/7_0_bst.cpp
@@ -13,9 +13,9 @@ protected:
     };
     Node *root = nullptr;
     void dispCore(Node *current, int level) {
-        for (int i = 0; i < level; i++) cout << "\t";
-        cout << current->data << " :==> [" << (current->left ? to_string(current->left->data) :"")
-                << ", " << (current->right ? to_string(current->right->data) : "") << "]" << endl;
+        for (int i = 0; i < level; i++) std::cout << "\t";
+        std::cout << current->data << " :==> [" << (current->left ? std::to_string(current->left->data) :"")
+                << ", " << (current->right ? std::to_string(current->right->data) : "") << "]" << std::endl;
         if (current->left) dispCore(current->left, level + 1);
         if (current->right) dispCore(current->right, level + 1);
     }
@@ -23,7 +23,7 @@ protected:
         if (!current) return new Node(val);
         else if (val > current->data) current->right = _insert(current->right, val);
         else if (val < current->data) current->left = _insert(current->left, val);
-        else throw string{"Error: Duplicate values not allowed in BST"};
+        else throw std::string{"Error: Duplicate values not allowed in BST"};
         return current;
     }
     bool _isPresent(Node *current, T val) {
@@ -31,7 +31,7 @@ protected:
         if (current->data == val) return true;
         else if (val > current->data) return _isPresent(current->right, val);
         else if (val < current->data) return _isPresent(current->left, val);
-        throw __FUNCTION__ + string{"Error: Control should not be here! All values of val are covered."};
+        throw __FUNCTION__ + std::string{"Error: Control should not be here! All values of val are covered."};
     }
 public:
     void disp() {
@@ -44,11 +44,16 @@ public:
     bool isPresent(T val) {
         return _isPresent(root, val);
     }
+    void InsertFromContainer(const auto &beg, const auto &end) {
+        for (auto it = beg; it != end; it++) {
+            insert(*it);
+        }
+    }
 };
 
 int main() {
     cout << boolalpha;
-    vector<int> v {43, 22, 49, 82, 19, 42, 81, 33, 22};
+    vector<int> v {43, 22, 49, 82, 19, 42, 81, 33, 222};
     BST<int> bst;
     try {
         for (auto elem : v) {
@@ -61,6 +66,7 @@ int main() {
         cout << "Error: the following exception was thrown:" << endl;
         cout << err << endl;
     }
+
  
     return 0;
 }
